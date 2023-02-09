a17cd718c3105b6dfb6d1eb1bed3dea2a16a48e7
samfrexz619 <samueloyawale315@gmail.com>
2023-02-09 13:33:35 +0100

    add axios and other packages

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { createApp } from 'vue'
+import store from './store'
 import App from './App.vue'
 import router from './router'
+
 
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(router)
-
+app.use(store)
 app.mount('#app')
